5f1c38a5ae68c44586269c24e2fa96eb8ea98c5d
jenniferlai43 <jenniferlai43@gmail.com>
2016-11-07 09:54:22 -0800

    Update Passenger.java

--- Passenger.java
+++ Passenger.java
@@ -8,15 +8,14 @@
 public class Passenger
 {
     private String firstName, lastName;
-    private int preference; //0 for none, 1 for first class, 2 for window
-    private int seatRow, seatColumn;
     
-    public Passenger(String first, String last, int sCol, int sRow) {
+    public Passenger(String first, String last) {
         firstName = first;
         lastName = last;
-        seatRow = sRow;
-        seatColumn = sCol;
-        preference = 0;
+    }
+    
+    public String getFullName() {
+        return firstName + lastName;
     }
     
     public String getFirstName() {
@@ -25,17 +24,5 @@ public class Passenger
     
     public String getLastName() {
         return lastName;
-    }
-    
-    public int getSeatRow() {
-        return seatRow;
-    }
-    
-    public int getSeatColumn() {
-        return seatColumn;
-    }
-    
-    public int getPreference() {
-        return preference;
     }
 }
