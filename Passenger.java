f6df93472e2d638f6f679b0d0b5c0cccd1751ea6
jenniferlai43 <jenniferlai43@gmail.com>
2016-11-06 18:21:09 -0800

    Update Passenger.java

--- Passenger.java
+++ Passenger.java
@@ -1,33 +1,41 @@
 
 /**
- * Write a description of class Passenger here.
+ * Passenger of Airplane.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
 public class Passenger
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Passenger
-     */
-    public Passenger()
-    {
-        // initialise instance variables
-        x = 0;
+    private String firstName, lastName;
+    private int preference; //0 for none, 1 for first class, 2 for window
+    private int seatRow, seatColumn;
+    
+    public Passenger(String first, String last, int sCol, int sRow) {
+        firstName = first;
+        lastName = last;
+        seatRow = sRow;
+        seatColumn = sCol;
+        preference = 0;
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    
+    public String getFirstName() {
+        return firstName;
+    }
+    
+    public String getLastName() {
+        return lastName;
+    }
+    
+    public int getSeatRow() {
+        return seatRow;
+    }
+    
+    public int getSeatColumn() {
+        return seatColumn;
+    }
+    
+    public int getPreference() {
+        return preference;
     }
 }
