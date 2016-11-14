97f83c5a4afceee7ae68a26a1e5d161f956e8e10
jenniferlai43 <jenniferlai43@gmail.com>
2016-11-13 17:51:17 -0800

    Update Passenger.java

--- Passenger.java
+++ Passenger.java
@@ -1,33 +1,49 @@
-
+ 
 /**
- * Passenger of Airplane.
+ * Attributes of a passenger in a seat of an airplane.
  * 
- * String firstName, lastName
- * Passenger (String firstName, String lastName)
- * getFullName()
- * getFirstName()
- * getLastName()
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (Jennifer and Jason) 
+ * @version (11/13/16)
  */
 public class Passenger
 {
-    private String firstName, lastName;
+    private String firstName, lastName; //first name and last name of Passenger
     
+    /**
+     * Creates passenger object with first name and last name.
+     * (Postcondition: passenger is created with first and last name.)
+     * @param first first name of passenger
+     * @param last last name of passenger
+     * (Precondition: passenger has first and last name.)
+     */
     public Passenger(String first, String last) {
         firstName = first;
         lastName = last;
     }
     
+    /**
+     * Returns full name of passenger.
+     * @return returns full name of passenger.
+     * (Precondition: passenger has first and last name.)
+     */
     public String getFullName() {
         return firstName + " " + lastName;
     }
     
+    /**
+     * Returns first name of passenger.
+     * @return first name of passenger
+     * (Precondition: passenger has first and last name.)
+     */
     public String getFirstName() {
         return firstName;
     }
     
+    /**
+     * Returns last name of passenger.
+     * @return last name of passenger
+     * (Precondition: passenger has first and last name.)
+     */    
     public String getLastName() {
         return lastName;
     }
